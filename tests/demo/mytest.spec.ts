884caeafb93e4c9aa77ded1da07ead90ed4b5617
SandhyaAghav <sandhyaphad@gmail.com>
2026-05-19 15:00:14 +0530

    new test cases

---
+++ tests/demo/mytest.spec.ts
@@ -0,0 +1,14 @@
+import { test, expect } from "@playwright/test";
+
+test("Should load homepage with correct title", async ({ page }) => {
+  await page.goto("https://katalon-demo-cura.herokuapp.com/");
+
+  await expect(page).toHaveTitle("CURA Healthcare Service");
+
+  await expect(page.locator("//h1")).toHaveText("CURA Healthcare Service");
+});
+
+test.only("should demo locators" , async ({page})=>{
+
+
+});
